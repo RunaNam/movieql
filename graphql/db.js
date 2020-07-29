ea5d72e38d41392190f9b45606aa813d0858475c
2018029416 <nmj5012@naver.com>
2020-07-29 22:35:39 +0900

    0727~0729

---
+++ graphql/db.js
@@ -0,0 +1,64 @@
+let movies = [
+  {
+    id: 0,
+    name: "Nicolas",
+    score: 18,
+  },
+  {
+    id: 1,
+    name: "Jisu",
+    score: 18,
+  },
+  {
+    id: 2,
+    name: "Japan Guy",
+    score: 18,
+  },
+  {
+    id: 3,
+    name: "Daal",
+    score: 18,
+  },
+  {
+    id: 4,
+    name: "JD",
+    score: 18,
+  },
+  {
+    id: 5,
+    name: "moondaddi",
+    score: 18,
+  },
+  {
+    id: 6,
+    name: "flynn",
+    score: 18,
+  },
+];
+
+export const getMovies = () => movies;
+
+export const getById = (id) => {
+  const filteredMovies = movies.filter((movie) => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
+};
